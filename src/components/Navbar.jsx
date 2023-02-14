ac7b77f31496cc03750f9510e6a68fc0a636cd11
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-14 10:57:10 +0300

    add signin component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import {auth} from "../firebase";
+import {useAuthState} from "react-firebase-hooks/auth";
+
+const style = {
+  nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
+  heading: `text-white text-3xl`
+}
 
 const Navbar = () => {
+  const [user] = useAuthState(auth)
   return (
-    <div>
-
+    <div className={style.nav}>
+      <h1 className={style.heading}>Chat App</h1>
     </div>
   )
 }
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { getAuth } from "firebase/auth";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCArAu9MhKPJomT5L2iJL-un1YItBqMVDM",
+  authDomain: "chat-app-42bb8.firebaseapp.com",
+  projectId: "chat-app-42bb8",
+  storageBucket: "chat-app-42bb8.appspot.com",
+  messagingSenderId: "260979966849",
+  appId: "1:260979966849:web:71ad9d3d3596a746399e80"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
