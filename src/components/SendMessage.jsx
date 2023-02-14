41a1282983eb688ec53cca8b3b06c47a8876e86f
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-14 13:04:24 +0300

    add scroll.current.scrollIntoView({behavior: 'smooth'})

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {auth, db} from "../firebase";
+import {addDoc, collection, serverTimestamp} from 'firebase/firestore'
 
 const style = {
   form: `h-14 w-full max-w-[728px] flex text-xl absolute bottom-0`,
@@ -7,9 +9,33 @@ const style = {
 }
 
 const SendMessage = () => {
+  const [input, setInput] = useState('')
+
+  const sendMessage = async (e) => {
+    e.preventDefault()
+    if (input === '') {
+      alert('Please enter a valid message')
+      return
+    }
+    const {uid, displayName} = auth.currentUser
+    await addDoc(collection(db, 'messages'), {
+      text: input,
+      name: displayName,
+      uid,
+      timestamp: serverTimestamp()
+    })
+    setInput('')
+    scroll.current.scrollIntoView({behavior: 'smooth'})
+  }
+
   return (
-    <form className={style.form}>
-      <input className={style.input} type="text" placeholder='Message'/>
+    <form onSubmit={sendMessage} className={style.form}>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        className={style.input}
+        type="text"
+        placeholder='Message'/>
       <button className={style.button} type='submit'>Send</button>
     </form>
   )
